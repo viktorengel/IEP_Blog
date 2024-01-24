0a8711f882a894da35a96de98ef8dc5ad1bd8545
Viktor Rengel <viktorengel@hotmail.com>
2024-01-23 23:14:29 -0500

    Vistas y plantilla blade

---
+++ resources/views/cursos/create.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.plantilla')
+
+@section('title', 'Cursos create')
+
+@section('content')
+    <h1>En esta página podras crear cursos</h1>
+@endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.plantilla')
+
+@section('title', 'Home')
+
+@section('content')
+    <h1>Bienvenidos a la Página principal</h1>
+@endsection
