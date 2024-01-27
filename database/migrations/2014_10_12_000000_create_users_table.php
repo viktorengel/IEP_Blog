1753bf157ca5fad067bd2503ec09bd77f72c6c58
Viktor Rengel <viktorengel@hotmail.com>
2024-01-27 12:50:49 -0500

    Migraciones parte 1

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,13 +12,15 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->id(); //integer unsigned increment
+            $table->string('name'); //varchar 255char
+            //$table->text('descripcion'); //texto mayor a 255char
+            $table->string('email')->unique(); //No se puede repetir en la DB
             $table->timestamp('email_verified_at')->nullable();
+            //Siempre que tengamos un campo que pueda quedar vacio debemos colocar el método nullable();
             $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->rememberToken(); //Registra un token cuando colocamos el check de recordar credenciales
+            $table->timestamps(); //created_at updated_at
         });
     }
 
